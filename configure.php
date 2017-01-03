78b1517ee10427117ffc3c24039432232b3b052b
aarefjev <aarefjev@gmail.com>
2017-01-03 19:28:36 +0000

    Update configure.php

--- configure.php
+++ configure.php
@@ -2,15 +2,10 @@
 
 $db_host		= '127.0.0.1';
 $db_user		= 'root';
-$db_password	= 'developer010pass';
-$db_database	= 'loc.devtools';
+$db_password	= 'rootpassword';
+$db_database	= 'arte.devtools';
 
-/*
-$db_host		= 'mysql2110.cp.blacknight.com';
-$db_user		= 'u1058137_arteads';
-$db_password	= ';Irf6{IXgv';
-$db_database	= 'db1058137_arteads';
-*/
+
 
 $apache_vhosts_dir = '/etc/apache2/sites-available';
 $apache_vhosts_active_dir = '/etc/apache2/sites-enabled';
