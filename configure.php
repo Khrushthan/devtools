ef5c0427351568b9597e9bec05ea95c95dbd3c01
Khrushthan <98970557+Khrushthan@users.noreply.github.com>
2022-05-08 02:58:35 +0800

    Update configure.php

--- configure.php
+++ configure.php
@@ -1,13 +1,13 @@
 <?php
 
 $db_host	= '127.0.0.1';
-$db_user	= 'root';
+$db_user	= 'unroot';
 $db_password	= 'developer010pass';
 $db_database	= 'arte.devtools';
 
 
 
-$apache_vhosts_dir = '/etc/apache2/sites-available';
-$apache_vhosts_active_dir = '/etc/apache2/sites-enabled';
+apache_vhosts_dir = '/etc/apache/sites-available';
+apache_vhosts_active_dir = '/etc/apache/sites-enabled';
 
-$project_directory = '/var/www/html/arte.devtools';
+project_directory = '/var/www/html/arte.devtools';
